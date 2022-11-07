bf85ff4850664fb579ea369d59a89816fc276249
lili <3238367573.com>
2022-11-07 17:01:55 +0800

    w

--- src/components/struggle10.vue
+++ src/components/struggle10.vue
@@ -4,15 +4,23 @@
     imgButtom="/images/学业信息10/struggle.png"
     title="你的奋斗1"
     resource="数据来源：教务处"
-
   >
     <ul>
       <li>这一学年</li>
-      <li>学时最长的是：<span class="blueHighlight sizeHighlight">{{zcxskc}}</span></li>
-      <li>学时最短的是：<span class="blueHighlight sizeHighlight">{{zdxskc}}</span></li>
-      <li style="margin-top:50px"><span class="blueHighlight sizeHighlight">{{dkzgcjjs}}</span>
-        老师的<span class="blueHighlight sizeHighlight">{{dkzgcjkcm}}</span>课程</li>
-      <li>你得分<span class="numRedHighlight underline">{{dkzgcjxf}}</span>，是你这学年最高分的课程</li>
+      <li>
+        学时最长的是：<span class="blueHighlight sizeHighlight">{{
+          zcxskc
+        }}</span>
+      </li>
+      <li>
+        学时最短的是：<span class="blueHighlight sizeHighlight">{{
+          zdxskc
+        }}</span>
+      </li>
+      <li style="margin-top: 50px">得分排名依次是:</li>
+      <li v-for="(item, key) in courseArr" :key="key">
+        <span class="blueHighlight sizeHighlight"> {{ item }}</span>
+      </li>
     </ul>
   </Common>
 </template>
@@ -20,8 +28,14 @@
 <script setup>
 import { reactive } from "@vue/reactivity";
 import Common from "./common.vue";
-import request from "../request"
-const {data:{zcxskc,zdxskc,dkzgcjjs,dkzgcjkcm,dkzgcjxf}}=await  request("/myterm/score",{})
+import request from "../request";
+import { computed } from "@vue/runtime-core";
+const {
+  data: { zcxskc, zdxskc, zgjdkc="" },
+} = await request("/myterm/score", {});
+const courseArr = computed(() => {
+  return zgjdkc.split("、").slice(0, 3);
+});
 </script>
 
 <style scoped>
