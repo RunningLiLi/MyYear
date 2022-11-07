e32a89cd08d177d67f2322fb002b17e44aa7b162
lili <3238367573.com>
2022-11-07 18:52:01 +0800

    w

--- src/request.js
+++ src/request.js
@@ -1,6 +1,6 @@
 export default function (path, header) {
   const token=window.location.search.replace("?token=","")
-  const baseUrl="http://172.20.2.60:5122";
+  const baseUrl="http://172.20.2.60:80";
   return fetch(baseUrl+path, {
     method:"GET",
     headers: {
