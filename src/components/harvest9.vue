2f4fd465f391ab6f93fd33348635e768f82c763f
lili <3238367573.com>
2022-11-14 18:48:14 +0800

    bug

--- src/components/harvest9.vue
+++ src/components/harvest9.vue
@@ -22,7 +22,7 @@
         <li>获得了</li>
         <li>
          <span 
-         v-for="(item,key) in zw" 
+         v-for="(item,key) in ry" 
          :key="key" 
          class="collageHighlight blueHighlight sizeHighlight">
          {{item}},
@@ -36,11 +36,14 @@
     
     <ul v-if="jz[0]">
         <li>&lt;奖助学金&gt;</li>
-        <li>同时，你获得了</li>
-        <li><span class="collageHighlight blueHighlight sizeHighlight">学业奖学金一等奖</span></li>
-        <li>所在的班级 <span class="numBlueHighlight">01011905</span></li>
-        <li>荣获了
-          <span v-for="(item,key) in jz" :key="key" 
+        <li v-if="jz[0]">同时，你获得了</li>
+        <li v-if="jz[0]"><span 
+        class="collageHighlight blueHighlight sizeHighlight"
+        v-for="(item,key) in jz" :key="key"
+        >{{item}}</span></li>
+        <li v-if="jt[0]">所在的班级 <span class="numBlueHighlight">{{bj}}</span></li>
+        <li v-if="jt[0]">荣获了
+          <span v-for="(item,key) in jt" :key="key" 
           class="collageHighlight blueHighlight sizeHighlight">{{item}}</span>
         </li>
     </ul>
@@ -55,6 +58,7 @@
 import Common from "../components/common.vue";
 import request from "../request"
 const {data:{zw,ry,jt,jz}}=await  request("/myterm/reward",{})
+const {data:{bj}}=await  request("/myterm/information",{})
 </script>
 
 <style scoped>
--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     }
   },
   server:{
-    host:"127.0.0.1",
+    host:"192.168.43.81",
     port:"5173",
     proxy: {
       "/api": {
